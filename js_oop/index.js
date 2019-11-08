0469105855d7682253c3c41faae1b9375786e2eb
farhazzanik <48301388+farhazzanik@users.noreply.github.com>
2019-11-08 14:46:00 +0200

    function work like a object

--- js_oop/index.js
+++ js_oop/index.js
@@ -63,3 +63,29 @@ function myNew(cons){
 
 var rect5 = myNew(Rectangle,14,15)
 rect5.draw()
+
+//fuction are object same
+
+function func(){
+	console.log(dd);
+}
+
+console.log(typeof(func))
+
+var Rect = new Function('height','width',`this.height = height
+			this.width = width
+
+
+			this.draw = function(){
+				console.log("I am a recetangle")
+				this.printProperties()
+				
+			}	
+
+			this.printProperties = function(){
+				console.log('My width is ' + this.width + ' My height is ' + this.height )
+			}`);
+
+
+var rect6 = new Rect(4,5)
+console.log(rect6);
