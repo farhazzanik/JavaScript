d82f48d360e098794159269cf4658e77a5249a0a
farhazzanik <48301388+farhazzanik@users.noreply.github.com>
2019-11-10 23:52:41 +0200

    custom inheritence in java script object create

--- js_oop/index.js
+++ js_oop/index.js
@@ -1,203 +1,213 @@
 //factory pattern
-var createRect = function(height, width) {
+// var createRect = function(height, width) {
 
-    return {
-        height: height,
-        width: width,
+//     return {
+//         height: height,
+//         width: width,
 
 
-        draw: function() {
-            console.log("I am a recetangle")
-            this.printProperties()
+//         draw: function() {
+//             console.log("I am a recetangle")
+//             this.printProperties()
 
-        },
+//         },
 
-        printProperties: function() {
-            console.log('My width is ' + this.width + ' My height is ' + this.height)
-        }
-    }
-}
+//         printProperties: function() {
+//             console.log('My width is ' + this.width + ' My height is ' + this.height)
+//         }
+//     }
+// }
 
 
-var rect1 = createRect(10, 20)
-rect1.draw()
-var rect2 = createRect(25, 56)
-rect2.draw()
-var rect3 = createRect(30, 258)
-rect3.draw()
+// var rect1 = createRect(10, 20)
+// rect1.draw()
+// var rect2 = createRect(25, 56)
+// rect2.draw()
+// var rect3 = createRect(30, 258)
+// rect3.draw()
 
-//constructor pattern
-var Rectangle = function(height, width) {
+// //constructor pattern
+// var Rectangle = function(height, width) {
 
-    this.height = height
-    this.width = width
+//     this.height = height
+//     this.width = width
 
 
-    this.draw = function() {
-        console.log("I am a recetangle")
-        this.printProperties()
+//     this.draw = function() {
+//         console.log("I am a recetangle")
+//         this.printProperties()
 
-    }
+//     }
 
-    this.printProperties = function() {
-        console.log('My width is ' + this.width + ' My height is ' + this.height)
-    }
+//     this.printProperties = function() {
+//         console.log('My width is ' + this.width + ' My height is ' + this.height)
+//     }
 
-}
+// }
 
-var rect4 = new Rectangle(10, 25)
-rect4.draw()
+// var rect4 = new Rectangle(10, 25)
+// rect4.draw()
 
 
 
-//how new keyword work
+// //how new keyword work
 
-function myNew(cons) {
-    var obj = {}
-    Object.setPrototypeOf(obj, cons.prototype)
-    var argsArray = Array.prototype.slice.apply(arguments)
-    cons.apply(obj, argsArray.slice(1))
-    return obj
-}
+// function myNew(cons) {
+//     var obj = {}
+//     Object.setPrototypeOf(obj, cons.prototype)
+//     var argsArray = Array.prototype.slice.apply(arguments)
+//     cons.apply(obj, argsArray.slice(1))
+//     return obj
+// }
 
 
-var rect5 = myNew(Rectangle, 14, 15)
-rect5.draw()
+// var rect5 = myNew(Rectangle, 14, 15)
+// rect5.draw()
 
-//fuction are object same
+// //fuction are object same
 
-function func() {
-    console.log(dd);
-}
+// function func() {
+//     console.log(dd);
+// }
 
-console.log(typeof(func))
+// console.log(typeof(func))
 
-var Rect = new Function('height', 'width', `this.height = height
-			this.width = width
+// var Rect = new Function('height', 'width', `this.height = height
+// 			this.width = width
 
 
-			this.draw = function(){
-				console.log("I am a recetangle")
-				this.printProperties()
-				
-			}	
+// 			this.draw = function(){
+// 				console.log("I am a recetangle")
+// 				this.printProperties()
 
-			this.printProperties = function(){
-				console.log('My width is ' + this.width + ' My height is ' + this.height )
-			}`);
+// 			}	
 
+// 			this.printProperties = function(){
+// 				console.log('My width is ' + this.width + ' My height is ' + this.height )
+// 			}`);
 
-var rect6 = new Rect(4, 5)
-console.log(rect6);
 
+// var rect6 = new Rect(4, 5)
+// console.log(rect6);
 
-//bind call apply function details
-//call
 
-var obj = {
-    a: 45,
-    b: 15,
-}
+// //bind call apply function details
+// //call
 
-function myFunc(c, d) {
-    console.log(this)
-    console.log(this.a + this.b + c + d)
-}
+// var obj = {
+//     a: 45,
+//     b: 15,
+// }
 
-myFunc.call(obj, 10, 15)
+// function myFunc(c, d) {
+//     console.log(this)
+//     console.log(this.a + this.b + c + d)
+// }
 
-//applly
-myFunc.apply(obj, [5, 6])
+// myFunc.call(obj, 10, 15)
 
+// //applly
+// myFunc.apply(obj, [5, 6])
 
-//bind method
-var test = myFunc.bind(obj)
 
-test(8, 9)
+// //bind method
+// var test = myFunc.bind(obj)
 
-//call by value and call by reference
-//pass by value and pass by reference
+// test(8, 9)
 
-var n = 10
+// //call by value and call by reference
+// //pass by value and pass by reference
 
-function change(n) {
-    n = n + 100
-    console.log(n)
-}
+// var n = 10
 
+// function change(n) {
+//     n = n + 100
+//     console.log(n)
+// }
 
-change(n)
-console.log(n)
 
-var NewObj = {
-    a: 10,
-}
+// change(n)
+// console.log(n)
 
-function MyChange(n) {
-    NewObj.a = NewObj.a + 100
-    console.log(NewObj.a)
-}
+// var NewObj = {
+//     a: 10,
+// }
 
-MyChange(NewObj.a)
+// function MyChange(n) {
+//     NewObj.a = NewObj.a + 100
+//     console.log(NewObj.a)
+// }
 
-console.log(NewObj.a)
+// MyChange(NewObj.a)
 
+// console.log(NewObj.a)
 
 
-// how abstraction work in javascript
-var Rectangle = function(height, width) {
 
-    var position = {
-        x: 10,
-        y: 20
-    }
+// // how abstraction work in javascript
+// var Rectangle = function(height, width) {
 
+//     var position = {
+//         x: 10,
+//         y: 20
+//     }
 
-    this.height = height
-    this.width = width
 
+//     this.height = height
+//     this.width = width
 
-    var printProperties = function() {
-        console.log('My width is ' + this.width + ' My height is ' + this.height)
-    }.bind(this)
 
-    this.getPostiton = function() {
-        return position
-    }
+//     var printProperties = function() {
+//         console.log('My width is ' + this.width + ' My height is ' + this.height)
+//     }.bind(this)
 
-    this.draw = function() {
-        console.log("I am a recetangle")
-        printProperties()
-        console.log("position x is " + position.x + " position y is " + position.y)
+//     this.getPostiton = function() {
+//         return position
+//     }
 
-    }
+//     this.draw = function() {
+//         console.log("I am a recetangle")
+//         printProperties()
+//         console.log("position x is " + position.x + " position y is " + position.y)
 
-    Object.defineProperty(this, 'position', {
-        get: function() {
-            return position
-        },
+//     }
 
-        set: function(value) {
-            position = value
-        }
-    })
+//     Object.defineProperty(this, 'position', {
+//         get: function() {
+//             return position
+//         },
 
+//         set: function(value) {
+//             position = value
+//         }
+//     })
 
-}
 
+// }
 
-var rect7 = new Rectangle(15, 16)
-rect7.draw()
-console.log(rect7.getPostiton())
-console.log(rect7.position)
-rect7.position = {
-    x: 58,
-    y: 56
-}
 
-console.log(rect7.position)
+// var rect7 = new Rectangle(15, 16)
+// rect7.draw()
+// console.log(rect7.getPostiton())
+// console.log(rect7.position)
+// rect7.position = {
+//     x: 58,
+//     y: 56
+// }
+
+// console.log(rect7.position)
 
 //instance vs prototype
+
+var Shape = function() {
+
+}
+
+Shape.prototype = {
+    common: function() {
+        console.log("this is common function")
+    },
+}
 
 
 var Square = function(width) {
@@ -212,20 +222,21 @@ var Square = function(width) {
 
 }
 
-Square.prototype = {
-    draw: function() {
-        console.log("this is Square")
-    },
-    toString: function() {
-        this.getWidth()
-        console.log("this is my width " + this.width)
-    }
+Square.prototype = Object.create(Shape.prototype)
+
+Square.prototype.draw = function() {
+    console.log("this is Square")
+}
+
+Square.prototype.toString = function() {
+    this.getWidth()
+    console.log("this is my width " + this.width)
 }
 
 
+
 var sqr1 = new Square(14)
-
 var sqr2 = new Square(15)
-
-console.log(sqr1)
-console.log(sqr2)
+var shape = new Shape()
+// console.log(sqr1)
+// console.log(sqr2)
