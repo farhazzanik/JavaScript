2ffb06ba155ab56f0c02b7143c5991c4d90f3e8a
farhazzanik <48301388+farhazzanik@users.noreply.github.com>
2019-11-11 23:30:05 +0200

    Iterator,for of loop,generator example in javascript

--- ES6_New/src/app.js
+++ ES6_New/src/app.js
@@ -1,43 +1,43 @@
 let arr = [1, 2, 3]
 
-//only accept for array es5 system
-// let createIterator = (collection) => {
+only accept for array es5 system
+let createIterator = (collection) => {
 
-//     let i = 0;
-//     return {
-//     	next() {
-//     		return {
-//     			done: i >= collection.length,
-//     			value : collection[i++]
-//     		}
-//     	}
-//     }
-// }
-
-
-// let interator = createIterator(arr)
+    let i = 0;
+    return {
+    	next() {
+    		return {
+    			done: i >= collection.length,
+    			value : collection[i++]
+    		}
+    	}
+    }
+}
 
 
-
-// let iterate = arr[Symbol.iterator]()
+let interator = createIterator(arr)
 
 
 
-// console.log(iterate.next())
-// console.log(iterate.next())
-// console.log(iterate.next())
-// console.log(iterate.next())
+let iterate = arr[Symbol.iterator]()
 
-// let str = 'Text'
-// let strIterator = str[Symbol.iterator]()
 
-// console.log(strIterator.next())
-// console.log(strIterator.next())
-// console.log(strIterator.next())
-// console.log(strIterator.next())
-// console.log(strIterator.next())
-// console.log(arr[Symbol.iterator])
-// console.log('str'[Symbol.iterator])
+
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+
+let str = 'Text'
+let strIterator = str[Symbol.iterator]()
+
+console.log(strIterator.next())
+console.log(strIterator.next())
+console.log(strIterator.next())
+console.log(strIterator.next())
+console.log(strIterator.next())
+console.log(arr[Symbol.iterator])
+console.log('str'[Symbol.iterator])
 
 
 for(let v of arr){
@@ -48,3 +48,68 @@ for(let v of arr){
 for (let v of 'Mahfuzul haque'){
 	console.log(v)
 }
+
+
+
+generator
+let obj = {
+    start: 1,
+    end: 5,
+    [Symbol.iterator]: function() {
+        let currentValue = this.start
+        const self = this
+        return {
+            next() {
+                return {
+                    done: currentValue > self.end,
+                    value: currentValue > self.end ? undifined : currentValue++
+                }
+            }
+        }
+
+    }
+
+    [Symbol.iterator]: function*() {
+        let currentValue = this.start
+        while (currentValue < this.end) {
+            yield currentValue++
+        }
+    }
+}
+
+let iterator = obj[Symbol.iterator]()
+
+console.log(iterator.next())
+console.log(iterator.next())
+console.log(iterator.next())
+console.log(iterator.next())
+console.log(iterator.next())
+
+
+function* generator() {
+    yield 1
+    yield 2
+    yield 3
+}
+
+
+let iter = generator();
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+
+
+function * generator(collection){
+	for (var i = 0; i <= collection.length; i++) {
+		yield collection[i]
+	}
+
+}
+
+
+let ite = generator(arr)
+console.log(ite.next())
+console.log(ite.next())
+console.log(ite.next())
+console.log(ite.next())
